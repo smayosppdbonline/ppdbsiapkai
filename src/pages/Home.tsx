499f447580a192e33cb60a0cb9783196677e1a80
smayosppdbonline <smayosppdbonline@gmail.com>
2026-04-27 12:37:37 +0900

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -221,7 +221,7 @@ export default function Home() {
           <div className="text-center mb-16">
             <h2 className="text-3xl md:text-4xl font-bold mb-4">Alur Pendaftaran PPDB</h2>
             <p className="text-slate-400 max-w-2xl mx-auto text-lg">
-              Ikuti langkah-langkah mudah berikut untuk mendaftarkan putra/putri Anda di SDN Harapan Bangsa.
+              Ikuti langkah-langkah mudah berikut untuk mendaftarkan putra/putri Anda di SMA Yosudarso Dobo.
             </p>
           </div>
 
